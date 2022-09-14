1ae07e91271f6990ddde416d6fe5820bbc26e565
onah GOdwin oko <onahgodwin773@gmail.com>
2022-09-14 15:12:11 -0700

    task 0-1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+*print_alphabet - Entry point
+*
+*Description: program that prints the alphabets
+*followed by a new line
+*
+*Return: Exit int function with an int 0
+*/
+voidprint_alphabet(void)
+{
+int aj;
+
+for (aj = 'a'; aj <= 'z'; aj++)
+{
+_putchar(aj);
+}
+_putchar('\n');
+
+}
